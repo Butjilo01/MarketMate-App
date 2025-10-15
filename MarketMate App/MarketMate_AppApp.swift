be5a145a96b4f6b7c5e3a0205292045a1d536675
Butjilo <dubebutjilo@gmail.com>
2025-10-15 12:21:36 +0200

    Initial Commit

---
+++ MarketMate App/MarketMate_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketMate_AppApp.swift
+//  MarketMate App
+//
+//  Created by admin on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarketMate_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
