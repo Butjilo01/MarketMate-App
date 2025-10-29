50c8c4603aac43e88452a736e95d29661f82c02e
Butjilo <dubebutjilo@gmail.com>
2025-10-29 10:40:38 +0200

    Modified some things on the home page and added the payment functionalities also added the history of payments but not fully functioning, also added the models and payment processor

---
+++ MarketMate App/History.swift
@@ -0,0 +1,18 @@
+//
+//  History.swift
+//  MarketMate App
+//
+//  Created by admin on 28/10/2025.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    History()
+}
---
+++ MarketMate App/PaymentProcessor.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentProcessor.swift
+//  MarketMate App
+//
+//  Created by admin on 28/10/2025.
+//
+
+import SwiftUI
+
+struct PaymentProcessor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentProcessor()
+}
